be8a2b6a2535e467f51ff2420b1a8a09bec9cbff
Administrator <Administrator@192.168.1.84>
2015-10-15 15:05:36 +0800

    class类

---
+++ git/src/com/pro/domain/Classes.java
@@ -0,0 +1,15 @@
+package com.pro.domain;
+
+public class Classes {
+	private int classId;
+
+	public int getClassId() {
+		return classId;
+	}
+
+	public void setClassId(int classId) {
+		this.classId = classId;
+	}
+	
+	
+}
